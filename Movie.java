d7de8ca76648b57f6d6b28d52e3ad59d61f2c5d6
riyeshbajracharya <rbajrac1@montgomerycollege.edu>
2026-03-03 23:04:24 -0500

    Add files via upload

--- Movie.java
+++ Movie.java
@@ -1,4 +1,4 @@
-package Task1;
+package Task2;
 public class Movie {
 
 	private String title;
--- MovieDriver.java
+++ MovieDriver.java
@@ -1,5 +1,4 @@
-package Task1;
-
+package Task2;
 import java.util.Scanner;
 
 public class MovieDriver {
@@ -7,23 +6,33 @@ public class MovieDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner (System.in);
-		Movie movie = new Movie();
+		
+		String option;
+		
+		do 
+		{
+			Movie movie = new Movie();
 			
-		System.out.println("Enter the name of a movie");
-		String title = input.nextLine();
-		movie.setTitle(title);
+			System.out.println("Enter the name of a movie");
+			String title = input.nextLine();
+			movie.setTitle(title);
 			
-		System.out.print("Enter the rating of the movie\n");
-		String rating = input.nextLine();
-		movie.setRating(rating);
+			System.out.print("Enter the rating of the movie\n");
+		    String rating = input.nextLine();
+		    movie.setRating(rating);
 		    
-		System.out.print("Enter the number of tickets sold for this movie\n");
-	    int tickets = input.nextInt();
-	    movie.setSoldTickets(tickets);
+		    System.out.print("Enter the number of tickets sold for this movie\n");
+	        int tickets = input.nextInt();
+	        movie.setSoldTickets(tickets);
 			
-	    input.nextLine();
+	        input.nextLine();
 	        
-	    System.out.println(movie.toString());
+	        System.out.println(movie.toString());
+	        
+	        System.out.println("Do you want to enter another? (y or n) ");
+	        option = input.nextLine();
+		
+		}while (option.equalsIgnoreCase("y"));
 		
         input.close();
         
